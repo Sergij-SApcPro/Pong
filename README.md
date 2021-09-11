e959c3f5d566315335d4c2af60f4784aba22683e
Sergij-SApcPro <sergijaleksovski@gmail.com>
2021-09-11 18:03:38 +0200

    Pong

---
+++ README.md
@@ -0,0 +1,67 @@
+<br/>
+<p align="center">
+  <a href="https://github.com/Sergij SApcPro/Pong">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">Pong by SApcPro</h3>
+
+  <p align="center">
+    A simple Pong game.
+    <br/>
+    <a href="https://github.com/Sergij SApcPro/Pong/issues">Report Bug</a>
+    .
+    <a href="https://github.com/Sergij SApcPro/Pong/issues">Request Feature</a>
+  </p>
+</p>
+
+## Table Of Contents
+
+* [About the Project](#about-the-project)
+* [Built With](#built-with)
+* [Getting Started](#getting-started)
+  * [Installation](#installation)
+* [Contributing](#contributing)
+* [Authors](#authors)
+
+## About The Project
+
+![Screen Shot](images/screenshot.png)
+
+This is a simple Pong game made by me (SApcPro) on Python. Pong is the first game that I made in Python.
+
+## Built With
+
+This cool game is built with Python. The Installer is built with NSIS
+
+## Getting Started
+
+### Installation
+
+1. Download the Installer;
+
+2. Run the installer and select the install destination;
+
+3. Click on Install;
+
+4. Run the game from the desktop shortcut.
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+* If you have suggestions for adding or removing projects, feel free to [open an issue](https://github.com/Sergij SApcPro/Pong/issues/new) to discuss it, or directly create a pull request after you edit the *README.md* file with necessary changes.
+* Please make sure you check your spelling and grammar.
+* Create individual PR for each suggestion.
+* Please also read through the [Code Of Conduct](https://github.com/Sergij SApcPro/Pong/blob/main/CODE_OF_CONDUCT.md) before posting your first idea as well.
+
+### Creating A Pull Request
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+## Authors
+
+* **Sergij Aleksovski SApcPro** - *Learning Programming.* - [Sergij Aleksovski SApcPro](https://github.com/Sergij-SApcPro) - **
---
+++ main.py
@@ -0,0 +1,119 @@
+import turtle
+import winsound
+
+win = turtle.Screen()
+win.title("Pong By SApcPro")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.2
+ball.dy = 0.2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("blue")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Roboto", 24, "normal"))
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 10
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 10
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 10
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 10
+    paddle_b.sety(y)
+
+# Keyboard binding
+win.listen()
+win.onkeypress(paddle_a_up, "w")
+win.onkeypress(paddle_a_down, "s")
+win.onkeypress(paddle_b_up, "Up")
+win.onkeypress(paddle_b_down, "Down")
+
+# Main Game Loop
+while True:
+    win.update()
+
+    # Move the Ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border Checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("ball.wav", winsound.SND_ASYNC)
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("ball.wav", winsound.SND_ASYNC)
+    
+    if ball.xcor() > 390:
+        ball.goto (0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Roboto", 24, "normal"))
+    
+    if ball.xcor() < -390:
+        ball.goto (0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Roboto", 24, "normal"))
+    
+    # Paaddle and Ball Collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -50):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() -50):
+        ball.setx(-340)
+        ball.dx *= -1
